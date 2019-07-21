d98e31a0ccaf7787162d0e490d2e9f233cbe1bd9
Louis Duret-Robert <louisduret@gmail.com>
2019-07-21 12:32:01 +0200

    launchfiles

--- src/MOBRO_ROS/config/turtlebot3_lds_2d_gazebo.lua
+++ src/MOBRO_ROS/config/turtlebot3_lds_2d_gazebo.lua
@@ -27,10 +27,10 @@ options = {
   use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
-  num_laser_scans = 0,
+  num_laser_scans = 1,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
-  num_point_clouds = 1,
+  num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
@@ -42,38 +42,16 @@ options = {
   landmarks_sampling_ratio = 1.,
 }
 
-MAP_BUILDER.use_trajectory_builder_3d = true
-MAP_BUILDER.num_background_threads = 30
+MAP_BUILDER.use_trajectory_builder_2d = true
 
-MAX_3D_LASER_RANGE = 3.5
-TRAJECTORY_BUILDER_3D.min_range = 0.1
-TRAJECTORY_BUILDER_3D.max_range = MAX_3D_RANGE
-TRAJECTORY_BUILDER_3D.low_resolution_adaptive_voxel_filter.max_range = MAX_3D_RANGE
-TRAJECTORY_BUILDER_3D.low_resolution_adaptive_voxel_filter.min_num_points = 500
-TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter.max_range = MAX_3D_RANGE
-TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter.min_num_points = 250
-TRAJECTORY_BUILDER_3D.submaps.high_resolution_max_range = MAX_3D_RANGE
+TRAJECTORY_BUILDER_2D.min_range = 0.1
+TRAJECTORY_BUILDER_2D.max_range = 3.5
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
+TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
 
-TRAJECTORY_BUILDER_3D.motion_filter.max_time_seconds = 0.25
-TRAJECTORY_BUILDER_3D.motion_filter.max_angle_radians = math.rad(0.1)
-TRAJECTORY_BUILDER_3D.submaps.high_resolution = 0.035
-TRAJECTORY_BUILDER_3D.submaps.low_resolution = 0.2
-
-TRAJECTORY_BUILDER_3D.ceres_scan_matcher.occupied_space_weight_0 = 10.
-TRAJECTORY_BUILDER_3D.ceres_scan_matcher.occupied_space_weight_1 = 15.
-TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight = 4.
-TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 1e3
-TRAJECTORY_BUILDER_3D.ceres_scan_matcher.only_optimize_yaw = true
-
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.2
-POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
-POSE_GRAPH.constraint_builder.min_score = 0.48
-POSE_GRAPH.constraint_builder.log_matches = true
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.min_rotational_score = 0.
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.linear_xy_search_window = 2.
-
-POSE_GRAPH.optimization_problem.huber_scale = 1e1
-POSE_GRAPH.optimization_problem.acceleration_weight = 1e-1
-POSE_GRAPH.optimization_problem.rotation_weight = 1e3
+POSE_GRAPH.constraint_builder.min_score = 0.65
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
 
 return options
--- src/MOBRO_ROS/config/turtlebot_urg_lidar_3d.lua
+++ src/MOBRO_ROS/config/turtlebot_urg_lidar_3d.lua
@@ -27,10 +27,10 @@ options = {
   use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
-  num_laser_scans = 1,
+  num_laser_scans = 0,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
-  num_point_clouds = 0,
+  num_point_clouds = 1,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
@@ -42,25 +42,38 @@ options = {
   landmarks_sampling_ratio = 1.,
 }
 
-MAP_BUILDER.use_trajectory_builder_2d = true
+MAP_BUILDER.use_trajectory_builder_3d = true
+MAP_BUILDER.num_background_threads = 30
 
-TRAJECTORY_BUILDER_2D.min_range = 0.1
-TRAJECTORY_BUILDER_2D.max_range = 8.
-TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.1
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 5.
-TRAJECTORY_BUILDER_2D.use_imu_data = false
-TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
+MAX_3D_LASER_RANGE = 3.5
+TRAJECTORY_BUILDER_3D.min_range = 0.1
+TRAJECTORY_BUILDER_3D.max_range = MAX_3D_RANGE
+TRAJECTORY_BUILDER_3D.low_resolution_adaptive_voxel_filter.max_range = MAX_3D_RANGE
+TRAJECTORY_BUILDER_3D.low_resolution_adaptive_voxel_filter.min_num_points = 500
+TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter.max_range = MAX_3D_RANGE
+TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter.min_num_points = 250
+TRAJECTORY_BUILDER_3D.submaps.high_resolution_max_range = MAX_3D_RANGE
 
-POSE_GRAPH.constraint_builder.min_score = 0.65
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
-POSE_GRAPH.constraint_builder.max_constraint_distance = 2.5
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.25
-POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 10000
-POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 10000
-POSE_GRAPH.matcher_translation_weight = 1000
-POSE_GRAPH.matcher_rotation_weight = 0.1
--- POSE_GRAPH.optimize_every_n_nodes = 0
+TRAJECTORY_BUILDER_3D.motion_filter.max_time_seconds = 0.25
+TRAJECTORY_BUILDER_3D.motion_filter.max_angle_radians = math.rad(0.1)
+TRAJECTORY_BUILDER_3D.submaps.high_resolution = 0.035
+TRAJECTORY_BUILDER_3D.submaps.low_resolution = 0.2
+
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.occupied_space_weight_0 = 10.
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.occupied_space_weight_1 = 15.
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight = 4.
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 1e3
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.only_optimize_yaw = true
+
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.2
+POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
+POSE_GRAPH.constraint_builder.min_score = 0.48
+POSE_GRAPH.constraint_builder.log_matches = true
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.min_rotational_score = 0.
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.linear_xy_search_window = 2.
+
+POSE_GRAPH.optimization_problem.huber_scale = 1e1
+POSE_GRAPH.optimization_problem.acceleration_weight = 1e-1
+POSE_GRAPH.optimization_problem.rotation_weight = 1e3
 
 return options
